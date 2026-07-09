578c6169bf5c5b1f2475f14b475059453a5bc8a4
Félix Piédallu <felix@piedallu.me>
2026-07-09 15:13:34 +0200

    paste: Data should only be text, not bytes. for simplification

--- src/yaste/paste.py
+++ src/yaste/paste.py
@@ -6,9 +6,9 @@ from pathlib import Path
 import zstandard
 
 
-def key_of_data(data: bytes) -> str:
+def key_of_data(data: str) -> str:
     m = hashlib.blake2b(digest_size=16)
-    m.update(data)
+    m.update(data.encode("utf-8"))
     return m.hexdigest()
 
 
@@ -28,22 +28,25 @@ class Paste:
     def exists(self, key: str) -> bool:
         return self.search(key) is not None
 
-    def read(self, path: Path) -> bytes:
-        data = path.read_bytes()
+    def read(self, path: Path) -> str:
         if path.name.endswith(".zst"):
-            data = zstandard.decompress(data)
+            data = zstandard.decompress(path.read_bytes()).decode("utf-8")
+        else:
+            data = path.read_text()
         return data
 
-    def create(self, data: bytes) -> str:
+    def create(self, data: str) -> str:
         key = key_of_data(data)
         if self.exists(key):
             raise FileExistsError(f"File with hash {key} already exists")
 
         if self._compress:
-            data = zstandard.compress(data, level=self._compress_level)
             file = self._path / f"{key}.zst"
+            file.write_bytes(
+                zstandard.compress(data.encode("utf-8"), level=self._compress_level)
+            )
         else:
             file = self._path / f"{key}.txt"
-        file.write_bytes(data)
+            file.write_text(data)
 
         return key

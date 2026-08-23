f49502c611b24e2d3d1d6b9ddf2016697cb2a9b1
Félix Piédallu <felix@piedallu.me>
2026-08-23 19:06:44 +0200

    frontend: embed most file types

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,11 @@
 import './assets/main.css'
 
 import 'highlight.js/styles/stackoverflow-light.css'
-import hljs from 'highlight.js/lib/core'
-import javascript from 'highlight.js/lib/languages/javascript'
+import hljs from 'highlight.js/lib/common';
 import hljsVuePlugin from '@highlightjs/vue-plugin'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
-// let url = "https://paste.yunohost.org/raw/ajoboxeluy";
-hljs.registerLanguage('javascript', javascript)
-hljs.registerLanguage('yaml', javascript)
 
 const app = createApp(App)
 app.use(hljsVuePlugin)
